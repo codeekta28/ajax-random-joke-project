9c1f19bd79a1a05394538e991f8fc184c2bb388d
Ekta <ektacode28@gmail.com>
2021-06-18 18:31:08 +0530

    concept is totally clear of AJAX

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log("This is app file");
+document.querySelector("#get-joke").addEventListener("click",()=>{
+let xhr = new XMLHttpRequest();
+ xhr.open("GET","https://api.chucknorris.io/jokes/random1",true);
+xhr.onprogress=function(){
+    console.log("onProgress");
+}
+xhr.onload = function(){
+    if(xhr.status===200 && this.readyState===4){
+        let jokes = JSON.parse(this.responseText)
+        console.log(jokes.value);
+        document.querySelector("#display-joke").innerHTML = jokes.value;
+    }else{
+        errorMsg();
+    }
+}
+xhr.send();
+function errorMsg(){
+    document.querySelector("#display-joke").innerHTML = `Oops Some Error`
+}
+})
